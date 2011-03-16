d21cf513ea7b091f7dc3222d573cc86b223d1c0c
Luke Palmer <lrpalmer@gmail.com>
2011-03-16 07:03:56 +0000

    Set up project to use remote postgres database instead of sqlite.

--- catalogserver/settings.py
+++ catalogserver/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.sqlite',              # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'codecatalog',              # Or path to database file if using sqlite3.
+        'USER': 'codecatalog',                      # Not used with sqlite3.
+        'PASSWORD': 'aB3$eF7*',                  # Not used with sqlite3.
+        'HOST': 'codecatalog.net',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
