2e55a47697009efd1b8fc3c0e41e7172ee01d31a
Luke Palmer <lrpalmer@gmail.com>
2011-03-19 22:20:03 -0600

    Added markdown editing for specs.

---
+++ catalogserver/templates/static/editable_markdown.js
@@ -0,0 +1,26 @@
+var makeMarkdownArea = function(textarea, divclass, editcallback){
+    var div = $('<div></div>');
+    div.addClass(divclass);
+    var markup = textarea.val();
+    var converter = new Showdown.converter();
+    var html = converter.makeHtml(markup);
+    div.append($(html));
+    
+    var editlink = $('<a href="#">edit</a>');
+    editlink.click(function() {
+        var newtextarea = $('<textarea rows="15" cols="72"></textarea>');
+        newtextarea.val(markup);
+        div.replaceWith(newtextarea);
+
+        var savebutton = $('<button>Save</button>');
+        savebutton.click(function() {
+            savebutton.remove();
+            editcallback(newtextarea.val());
+            makeMarkdownArea(newtextarea);
+        });
+        newtextarea.after(savebutton);
+    });
+    div.append(editlink);
+
+    textarea.replaceWith(div);
+};
