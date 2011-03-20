0c6764baac676a8663904ef3669c1cecc38a09d1
Luke Palmer <lrpalmer@gmail.com>
2011-03-19 20:12:52 -0600

    Added CodeCatalog tags to the copy-paste interface.

--- catalogserver/templates/static/select_on_click.js
+++ catalogserver/templates/static/select_on_click.js
@@ -1,6 +1,22 @@
-var select_on_click = function(elt) {
+var count_lines = function(text) {
+    return text.split(/\n/).length;
+};
+
+var select_on_click = function(elt, modify) {
     elt.focus(function() {
+        var text = elt.val();
+        elt.oldtext = text;
+        elt.oldrows = elt.attr('rows');
+        var newtext = modify(text);
+        elt.attr('rows', count_lines(newtext));
+        elt.val(newtext);
         elt.select();
+    });
+    elt.focusout(function() {
+        if (elt.oldtext) {
+            elt.attr('rows', elt.oldrows);
+            elt.val(elt.oldtext);
+        }
     });
     // keep chrome from immediately deselecting
     elt.mouseup(function() { return false });
