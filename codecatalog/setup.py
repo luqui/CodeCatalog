a9a8fbfc5981e4c19fab152bdf773f610206ef74
Luke Palmer <lrpalmer@gmail.com>
2011-03-23 18:20:17 -0600

    Added a setup script for codecatalog.  Renamed codecatalog.py to just codecatalog.

---
+++ codecatalog/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='CodeCatalog',
+    version='0.1',
+    description='A library and command line tool for interacting with the CodeCatalog site',
+    scripts=['src/codecatalog'],
+)
