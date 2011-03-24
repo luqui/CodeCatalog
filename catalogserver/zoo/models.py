0c32b24047294437c7c7de81493c4088f47c11b2
Luke Palmer <lrpalmer@gmail.com>
2011-03-23 20:32:39 -0600

    "Sync" command-line tool method.

--- catalogserver/zoo/models.py
+++ catalogserver/zoo/models.py
@@ -11,26 +11,17 @@ class Version(models.Model):
     active = models.BooleanField()    # cache field that always marks the latest approved version
     versionptr = models.ForeignKey(VersionPtr)
 
-
 class Spec(models.Model):
     version = models.OneToOneField(Version, primary_key=True)
     name = models.CharField(max_length=128)
     summary = models.TextField()
     spec = models.TextField()
 
-    # don't think this belongs here
-    def url(self):
-        return '/spec/' + str(self.id) + '/'
-
 class Snippet(models.Model):
     version = models.OneToOneField(Version, primary_key=True)
     code = models.TextField()
     language = models.TextField()
     spec_versionptr = models.ForeignKey(VersionPtr)
-
-    # don't think this belongs here
-    def url(self):
-        return '/' + str(self.id) + '/'
 
 class Vote(models.Model):
     user       = models.ForeignKey(User)
--- codecatalog/codecatalog.vim
+++ codecatalog/codecatalog.vim
@@ -1,1 +1,2 @@
 noremap <Leader>cp :!codecatalog post<CR>
+noremap <Leader>cs :!codecatalog scan<CR>
