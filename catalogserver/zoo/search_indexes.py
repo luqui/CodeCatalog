0bc478ecfde0ee17fc2ca4658145240ff5560f4e
Luke Palmer <lrpalmer@gmail.com>
2011-03-26 11:38:05 +0000

    I don't think whoosh can handle realtime mode.

--- catalogserver/zoo/search_indexes.py
+++ catalogserver/zoo/search_indexes.py
@@ -4,7 +4,7 @@ from zoo.models import Spec
 
 # TODO RealTimeSearchIndex will SUCK once things start getting going.
 # Got to switch to Solr backend or a lazier indexing scheme.
-class SpecIndex(RealTimeSearchIndex):
+class SpecIndex(SearchIndex):
     name = CharField(model_attr='name')
     summary = CharField(model_attr='summary')
     text = CharField(document=True, use_template=True)
