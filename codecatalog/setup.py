df2f782308633207aa3bcc6c8ad8a4d50c88ef45
Luke Palmer <lrpalmer@gmail.com>
2011-03-28 00:27:39 -0600

    Moved the module layout for the command line utility around.

--- codecatalog/setup.py
+++ codecatalog/setup.py
@@ -4,5 +4,6 @@ setup(
     name='CodeCatalog',
     version='0.1',
     description='A library and command line tool for interacting with the CodeCatalog site',
-    scripts=['src/codecatalog'],
+    packages=['codecatalog'],
+    scripts=['scripts/codecatalog'],
 )
